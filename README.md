d61192fbcad2c66c39a56c4a70b65d0272912018
Yee Yang Tan <yeeyang.tan@live.com>
2020-10-23 17:25:31 +0200

    update

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # win32 ignore pop up diaglogue for password
 
+To automate a lot of power point files, it is sometimes stucked with the power point files with password. The issue for this problem is stated here:
 https://stackoverflow.com/questions/51731292/python-win32com-powerpoint-application-skip-password-pop-up-dialogue
+
+The method is to throw a thread that detects for the 'Password' pop-up diaglogue and close it. If no 'Password' pop-up dialogue is detected, then the thread is terminated through a flag.
+
+The 'Password' pop-up dialogue of the PowerPoint is shown:
+
+![png](README_files/8vLRc.png)
+
+
+
+# Requirements
+
+- windows system
+- Microsoft PowerPoint is installed
--- ignore.py
+++ ignore.py
@@ -3,6 +3,30 @@
 Created on Mon Dec  3 11:52:24 2018
 
 @author: Yang
+
+
+MIT License
+
+Copyright (c) [2018] [Yee Yang, Tan] (yeeyang.tan@live.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 """
 
 import win32gui
@@ -23,28 +47,29 @@ def terminate():
             break
 
 
-filename = 'pw.pptx'
+if __name__ == "__main__":
 
+    filename = 'pw.pptx'
 
-PPTApplication = win32com.client.Dispatch("PowerPoint.Application")
-PPTApplication.DisplayAlerts = False
+    PPTApplication = win32com.client.Dispatch("PowerPoint.Application")
+    PPTApplication.DisplayAlerts = False
 
-print('thread start')
-t = threading.Thread(target=terminate)
-t.start()
+    print('thread start')
+    t = threading.Thread(target=terminate)
+    t.start()
 
-try:
-    print('open ppt')
-    PPTApplication.Presentations.Open(filename,ReadOnly=True,WithWindow=True)
-except:
-    print('exception error')
-    t.join()
-    None
+    try:
+        print('open ppt')
+        PPTApplication.Presentations.Open(filename,ReadOnly=True,WithWindow=True)
+    except:
+        print('exception error')
+        t.join()
+        None
 
-if t.is_alive():
-    print('thread alive')
-    flag = True
-    t.join()
-    print('thread dead')
+    if t.is_alive():
+        print('thread alive')
+        flag = True
+        t.join()
+        print('thread dead')
 
-print('done')
+    print('done')
